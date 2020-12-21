9a940bc2e2d13d249f478616e9411905f1e64b5e
Marazorq <shuratov2001@mail.ru>
2020-12-21 17:43:50 +0300

    final 1.0

--- Chess.php
+++ Chess.php
@@ -2,7 +2,7 @@
 
 	interface IChessmen
 	{
-		public function move();
+		public function move($x, $y);
 		public function getPosition();
 	}
 
@@ -11,30 +11,72 @@
 		public $x;
 		public $y;
 
-		public function getPosition($x, $y)
-		{
-			$this->x = $x;
-			$this->y = $y;
+		public function getPosition()
+		{	
+			$arr = array($this->x,$this->y);
+			return $arr;
 		}
 	}
 
 	class King extends AbstractChessmen
 	{
-		public function move()
+		public function __construct($X, $Y)
 		{
+			$this->x = $X;
+			$this->y = $Y;
+		}
 
+		final public function move($X, $Y)
+		{
+			$posX = abs($X - $this->x);
+            $posY = abs($Y - $this->y);
+            if (($posX < 2) && ($posY < 2) && ($X > 0) && ($X < 9) && ($Y > 0) && ($Y < 9))
+            {
+                $this->x = $X;
+                $this->y = $Y;
+            }
+            else
+            {
+                echo "erroro king </br>";
+                //die();
+            }
 		}
 	}
+
 
 	class Queen extends AbstractChessmen
 	{
-		public function move()
+		public function __construct($X, $Y)
 		{
-			
+			$this->x = $X;
+			$this->y = $Y;
+		}
+
+		final public function move($X, $Y)
+		{
+			$posX = abs($X - $this->x);
+            $posY = abs($Y - $this->y);
+            if ((($posX == $posY) || ($posY == 0) || ($posX == 0)) && ($X > 0) && ($X < 9) && ($Y > 0) && ($Y < 9))
+            {
+                $this->x = $X;
+                $this->y = $Y;
+            }
+            else
+            {
+                echo "erroro queen</br>";
+                //die();
+            }
 		}
 	}
 
+	$king = new King(4,3);
+	$king->move(3,3);
+    $res = $king->getPosition();
 
-	$king = new King(1,1);
-	$queen = new Queen(4,3);
+    var_dump($res);
+    echo "</br>";
+	$queen = new Queen(1,1);
+	$queen->move(6,6);
+
+	var_dump($queen->getPosition());
 
